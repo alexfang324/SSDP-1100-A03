d17182e3cc211e92dea04a28949ff3e2edbb256c
Alex Fang <106351391+alexfang324@users.noreply.github.com>
2023-09-29 13:37:51 -0700

    initial commit with folder structure and template files set up

---
+++ script/random-order.js
@@ -0,0 +1,6 @@
+// You can assume the outer wrapping div of the featured profile will have a class name "featured-profile"
+// and each of the regular profile having a class name "profile". I have imported this file into our-team.html
+// so you only have to implement the script here.
+
+// To be able to correctly reach into each profile and swap the content, you need to communicate closely
+// with Joshan to learn he is is structuring each profile in html.
