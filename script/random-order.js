60a942a0fde82c3d98cf745c3ad51c3ed3e22a7b
Amanda Mar <msamandamar@gmail.com>
2023-10-04 22:57:19 -0700

    javascript randomizer and corresponding HTML changes

--- script/random-order.js
+++ script/random-order.js
@@ -4,3 +4,24 @@
 
 // To be able to correctly reach into each profile and swap the content, you need to communicate closely
 // with Joshan to learn he is is structuring each profile in html.
+
+
+  // Function to randomize profile order
+  function randomizeProfileOrder() {
+    // Select all featured-profile and profile elements
+    const people = document.querySelectorAll('.person');
+    //select the featured profile
+    const featuredPerson = document.querySelector(".featured-profile");
+    // randomize picking a number between 0 and 4
+    let length = people.length;
+    var choice = Math.floor(Math.random() * length);
+    // select a random person's profile
+    var randomPerson = people[choice];
+    // commence the swapping
+    const placeholder = featuredPerson.innerHTML;
+    featuredPerson.innerHTML = randomPerson.innerHTML;
+    randomPerson.innerHTML = placeholder;
+  }
+// simply call the function because it gets executed everytime the page loads, since the script is attached at the bottom of the html document
+randomizeProfileOrder();
+  
